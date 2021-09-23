9db189fd7766d7350aaaf6d18095ab0cf7e19b3c
Bazza1897 <blagojaangov@gmail.com>
2021-09-23 19:53:44 +0200

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def home(request):
+    context = {
+        "posts": Post.objects.all(),
+        "title": "Blog Home"
+    }
+
+
+    return render(request, "blog/home.html", context=context)
+
+
+def about(request):
+    context = {
+        "title": "Blog About"
+    }
+
+    return render(request, "blog/about.html", context=context)
